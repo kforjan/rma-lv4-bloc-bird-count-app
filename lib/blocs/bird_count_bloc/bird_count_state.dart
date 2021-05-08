5f7392cf433374792a93ddb7e04bff46c7d85185
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-05-08 19:58:29 +0200

    Added bird count states

--- lib/blocs/bird_count_bloc/bird_count_state.dart
+++ lib/blocs/bird_count_bloc/bird_count_state.dart
@@ -3,4 +3,17 @@ part of 'bird_count_bloc.dart';
 @immutable
 abstract class BirdCountState {}
 
-class BirdCountInitial extends BirdCountState {}
+class BirdCountInitial extends BirdCountState {
+  final count = 0;
+  final color = Colors.blue;
+}
+
+class BirdCountIncreased extends BirdCountState {
+  BirdCountIncreased({
+    @required this.count,
+    @required this.color,
+  });
+
+  final int count;
+  final Color color;
+}
