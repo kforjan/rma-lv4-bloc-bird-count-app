f545ce9e2829ef17f83e728a7c037cda21dccb55
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-05-08 23:56:35 +0200

    Added shared prefs storing in bloc and fixed bloc dependencies

--- lib/blocs/bird_count_bloc/bird_count_bloc.dart
+++ lib/blocs/bird_count_bloc/bird_count_bloc.dart
@@ -4,21 +4,21 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'package:rma_lv4_bird_counting_bloc/data/sharedpref/shared_preferences_helper.dart';
-import 'package:rma_lv4_bird_counting_bloc/injection_container.dart' as di;
 
 part 'bird_count_event.dart';
 part 'bird_count_state.dart';
 
 class BirdCountBloc extends Bloc<BirdCountEvent, BirdCountState> {
-  BirdCountBloc() : super(BirdCountInitial());
+  BirdCountBloc(this.spHelper) : super(BirdCountInitial());
+
+  SharedPreferencesHelper spHelper;
 
   @override
   Stream<BirdCountState> mapEventToState(
     BirdCountEvent event,
   ) async* {
     if (event is LoadStoredCountData) {
-      final data =
-          await di.locator<SharedPreferencesHelper>().getStoredBirdData();
+      final data = await spHelper.getStoredBirdData();
       if (data['count'] != null && data['color'] != null) {
         yield BirdCountIncreased(count: data['count'], color: data['color']);
       } else {
@@ -26,23 +26,31 @@ class BirdCountBloc extends Bloc<BirdCountEvent, BirdCountState> {
       }
     } else if (event is ResetCounter) {
       yield BirdCountInitial();
-      di.locator<SharedPreferencesHelper>().deleteAllData();
+      spHelper.deleteAllData();
     } else if (event is IncreaseBrown) {
+      spHelper.saveBirdCount(state.count + 1);
+      spHelper.saveLastBirdColor('brown');
       yield BirdCountIncreased(
         count: state.count + 1,
         color: Colors.brown,
       );
     } else if (event is IncreaseGrey) {
+      spHelper.saveBirdCount(state.count + 1);
+      spHelper.saveLastBirdColor('grey');
       yield BirdCountIncreased(
         count: state.count + 1,
         color: Colors.grey,
       );
     } else if (event is IncreaseBlack) {
+      spHelper.saveBirdCount(state.count + 1);
+      spHelper.saveLastBirdColor('black');
       yield BirdCountIncreased(
         count: state.count + 1,
         color: Colors.black,
       );
     } else if (event is IncreaseOrange) {
+      spHelper.saveBirdCount(state.count + 1);
+      spHelper.saveLastBirdColor('orange');
       yield BirdCountIncreased(
         count: state.count + 1,
         color: Colors.orange,
