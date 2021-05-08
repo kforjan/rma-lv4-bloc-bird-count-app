574debc35ffa1d412aab2ed83564bd0381e2ecdf
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-05-08 20:01:26 +0200

    Added bird count events

--- lib/blocs/bird_count_bloc/bird_count_event.dart
+++ lib/blocs/bird_count_bloc/bird_count_event.dart
@@ -2,3 +2,13 @@ part of 'bird_count_bloc.dart';
 
 @immutable
 abstract class BirdCountEvent {}
+
+class IncreaseBrown extends BirdCountEvent {}
+
+class IncreaseGrey extends BirdCountEvent {}
+
+class IncreaseBlack extends BirdCountEvent {}
+
+class IncreaseOrange extends BirdCountEvent {}
+
+class ResetCounter extends BirdCountEvent {}
